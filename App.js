8b286ed03a3774700736bf5f13e03ff5b2edde3f
diBasch <barbash.dv@gmail.com>
2020-12-07 20:33:30 +0300

    Add button, logo

--- App.js
+++ App.js
@@ -1,36 +1,51 @@
 import React, { useState } from 'react'
-import { View, Text, StyleSheet, Image, TextInput } from 'react-native'
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TextInput,
+  TouchableWithoutFeedback,
+  Keyboard,
+  TouchableOpacity,
+} from 'react-native'
 
 const App = () => {
   const [name, setName] = useState('')
   const [password, setPassword] = useState('')
   return (
-    <View style={styles.container}>
-      <Image
-        style={{ width: 200, height: 200, marginBottom: 20 }}
-        source={require('./assets/items/logo.png')}
-      />
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.container}>
+        <Image
+          style={{ width: 200, height: 200, marginBottom: 20 }}
+          source={require('./assets/items/logo.png')}
+          resizeMode="contain"
+        />
 
-      <TextInput
-        style={styles.textInput}
-        placeholderTextColor="grey"
-        placeholder="name"
-        onChangeText={(value) => setName(value.toString())}
-        value={name}
-      />
-      <TextInput
-        style={styles.textInput}
-        placeholderTextColor="grey"
-        placeholder="password"
-        secureTextEntry
-        onChangeText={(value) => setPassword(value.toString())}
-        value={password}
-      />
+        <TextInput
+          style={styles.textInput}
+          placeholderTextColor="grey"
+          placeholder="name"
+          selectionColor="black"
+          onChangeText={(value) => setName(value.toString())}
+          value={name}
+        />
+        <TextInput
+          style={styles.textInput}
+          placeholderTextColor="grey"
+          placeholder="password"
+          selectionColor="black"
+          secureTextEntry
+          onChangeText={(value) => setPassword(value.toString())}
+          value={password}
+        />
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.textButton}>Sign In</Text>
+        </TouchableOpacity>
 
-      <Text style={{ marginTop: 150, color: 'grey' }}>
-        diBasch test for skyweb
-      </Text>
-    </View>
+        <Text style={styles.text}>diBasch test for skyweb</Text>
+      </View>
+    </TouchableWithoutFeedback>
   )
 }
 
@@ -48,9 +63,28 @@ const styles = StyleSheet.create({
     borderColor: 'lightgrey',
     borderRadius: 10,
     borderWidth: 1,
+    borderStyle: 'dashed',
     fontSize: 18,
     color: 'black',
     textAlign: 'center',
+  },
+  text: {
+    marginTop: 150,
+    color: 'grey',
+  },
+  button: {
+    marginVertical: 20,
+    width: '80%',
+    height: 50,
+    borderColor: 'lightgrey',
+    borderRadius: 10,
+    borderWidth: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textButton: {
+    fontSize: 18,
+    color: 'black',
   },
 })
 
