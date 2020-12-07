6ab28b59044c4ad57694b9d02aea9cdff0a4c5b2
diBasch <barbash.dv@gmail.com>
2020-12-07 20:21:35 +0300

    setup ios/android, add folders, add logo screen

--- App.js
+++ App.js
@@ -1,10 +1,35 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, StyleSheet, Image, TextInput } from 'react-native'
 
 const App = () => {
+  const [name, setName] = useState('')
+  const [password, setPassword] = useState('')
   return (
     <View style={styles.container}>
-      <Text>test for skyweb</Text>
+      <Image
+        style={{ width: 200, height: 200, marginBottom: 20 }}
+        source={require('./assets/items/logo.png')}
+      />
+
+      <TextInput
+        style={styles.textInput}
+        placeholderTextColor="grey"
+        placeholder="name"
+        onChangeText={(value) => setName(value.toString())}
+        value={name}
+      />
+      <TextInput
+        style={styles.textInput}
+        placeholderTextColor="grey"
+        placeholder="password"
+        secureTextEntry
+        onChangeText={(value) => setPassword(value.toString())}
+        value={password}
+      />
+
+      <Text style={{ marginTop: 150, color: 'grey' }}>
+        diBasch test for skyweb
+      </Text>
     </View>
   )
 }
@@ -14,7 +39,18 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#2c3e50',
+    backgroundColor: 'white',
+  },
+  textInput: {
+    marginVertical: 20,
+    width: '80%',
+    height: 50,
+    borderColor: 'lightgrey',
+    borderRadius: 10,
+    borderWidth: 1,
+    fontSize: 18,
+    color: 'black',
+    textAlign: 'center',
   },
 })
 
