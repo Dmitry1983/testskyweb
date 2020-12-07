0fdf346f2982297080bd58bba64453c06437c396
diBasch <barbash.dv@gmail.com>
2020-12-07 19:26:15 +0300

    first commit

---
+++ ios/Pods/Target Support Files/Pods-testskyweb-testskywebTests/Pods-testskyweb-testskywebTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_testskyweb_testskywebTests : NSObject
+@end
+@implementation PodsDummy_Pods_testskyweb_testskywebTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-testskyweb-tvOSTests/Pods-testskyweb-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_testskyweb_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_testskyweb_tvOSTests
+@end
